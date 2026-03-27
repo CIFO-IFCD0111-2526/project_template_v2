9252ea7dcfc8dec3bd240c10f284e40923e0d11d
Alexandre <alxnrocha@gmail.com>
2026-03-27 20:31:43 +0100

    feat: formulario login con validación

--- front/scripts.js
+++ front/scripts.js
@@ -1,2 +1,33 @@
-// TODO: Equipo Front JS - implementar logica aqui
-// Ver issues en GitHub para las tareas asignadas
+const forms = document.querySelectorAll("form");
+const signInRES = document.querySelector("#signInRES");
+
+forms[0].addEventListener("submit", async e => {
+    e.preventDefault();
+
+    try {
+        const res = await fetch("/api/v1/signin", {
+            method: "post",
+            body: JSON.stringify({
+                email: e.target.emailSignIn.value,
+                password: e.target.passwordSignIn.value
+            }),
+            headers: { "Content-Type": "application/json" },
+        });
+        const resJSON = await res.json();
+
+        if (resJSON.error) {
+            signInRES.textContent = resJSON.error;
+        } else {
+            signInRES.textContent = resJSON.message;
+
+            if (resJSON.accessToken) {
+                localStorage.setItem("accessToken", resJSON.accessToken);
+            }
+            setTimeout(() => {
+                window.location.href = "/private";
+            }, 3000);
+        }
+    } catch (error) {
+        signInRES.textContent = "Error de connectión con el servidor.";
+    }
+});
