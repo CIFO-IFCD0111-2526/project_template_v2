6a09a275dad58d8ce08caf86d13103f5980a9fb9
TelesforoSol <telesforosolc@gmail.com>
2026-03-27 20:23:27 +0100

    feat: signup form js

--- front/scripts.js
+++ front/scripts.js
@@ -1,2 +1,23 @@
 // TODO: Equipo Front JS - implementar logica aqui
 // Ver issues en GitHub para las tareas asignadas
+const forms = document.querySelectorAll("form");
+const signUpRES = document.querySelector("#signUpRES");
+// const privateLink = document.querySelectorAll("a");
+
+
+// registro
+forms[1].addEventListener("submit", async e => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3000/signup", {
+        method: "post",
+        body: JSON.stringify({ 
+            email: e.target.emailSignUp.value,
+            password: e.target.passwordSignUp.value
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+    const resJSON = await res.json();
+
+    if (resJSON.error) signUpRES.textContent = resJSON.error;
+    else signUpRES.textContent = resJSON.message;
+});
