f470be499a725bb1715f25fc5ebd57c56a19a0e7
Alexandre <alxnrocha@gmail.com>
2026-03-30 19:43:32 +0200

    feat: form login add validación joi

--- front/scripts.js
+++ front/scripts.js
@@ -1,33 +1,49 @@
-const forms = document.querySelectorAll("form");
-const signInRES = document.querySelector("#signInRES");
+
+const forms = document.querySelectorAll("form")
+const signInRES = document.querySelector("signInRES")
+
+const UserSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required()
+});
 
 forms[0].addEventListener("submit", async e => {
     e.preventDefault();
+    const email = e.target.email.emailSignIn.value
+    const password = e.target.passwordSignIn.value
+
+    const { error } = UserSchema.validate({ email, password })
+
+    if (error) {
+        signInRES.textContent = "Email inválido ou password menor que 6 caracteres."
+        return
+    }
 
     try {
         const res = await fetch("/api/v1/signin", {
             method: "post",
-            body: JSON.stringify({
-                email: e.target.emailSignIn.value,
-                password: e.target.passwordSignIn.value
-            }),
-            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({email, password}),
+            headers: {"Content-Type": "application/json"}
         });
+
         const resJSON = await res.json();
 
         if (resJSON.error) {
             signInRES.textContent = resJSON.error;
+
         } else {
             signInRES.textContent = resJSON.message;
 
             if (resJSON.accessToken) {
                 localStorage.setItem("accessToken", resJSON.accessToken);
             }
+
             setTimeout(() => {
-                window.location.href = "/private";
+                window.location.href = "/private"
             }, 3000);
         }
+
     } catch (error) {
-        signInRES.textContent = "Error de connectión con el servidor.";
+        signInRES.textContent = "Error de connectión con el servidor."
     }
-});
+})
