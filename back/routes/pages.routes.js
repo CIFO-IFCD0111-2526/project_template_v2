5b1025eba34df14e5a884cbcd74d1e987ee408b3
SinMasGato <sejodiotodoportuculpa@gmail.com>
2026-04-01 18:33:13 +0200

    feat: ruta página /me en server.js

--- back/routes/pages.routes.js
+++ back/routes/pages.routes.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const router = new express.Router();
+const path = require("path");
+const { authPage } = require("../auth.middleware");
 
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front/index.html"));
+});
 // Ruta-> para la pagina de Tareas
 // GET /private -> /private -> private.html
 server.get("/private", authPage, (request,response) => {
@@ -6,7 +13,9 @@ server.get("/private", authPage, (request,response) => {
 });
 
 // Ruta-> Pagina de tareas (Privada)
-// GET /account -> Sirve front/account.html
-server.get("/account", authPage, (request,response) =>{
+// GET /me -> Sirve front/me.html
+server.get("/me", authPage, (request,response) =>{
     response.sendFile(path.join(__dirname, '..' , 'front' , 'me.html'));
 } );
+
+module.exports = router;
