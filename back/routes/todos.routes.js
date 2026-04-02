6b513b6c0c30699982cfe35044130fdb34aa99d9
Alexandre <alxnrocha@gmail.com>
2026-04-02 18:50:29 +0200

    feat: API - borrar tarea - DELETE /api/v1/todos/:id #66

--- back/routes/todos.routes.js
+++ back/routes/todos.routes.js
@@ -4,8 +4,10 @@ const router = new express.Router();
 const pool = require("../mysql_conn.js");
 const { authAPI } = require("../auth.middleware.js");
 
+// ------- -> GET <- -------
+
 router.get("/todos", authAPI, async (req, res) => {
-  const { email } = req.data;
+  const {email} = req.data;
 
   try {
     const [rows] = await pool.query(
@@ -17,11 +19,41 @@ router.get("/todos", authAPI, async (req, res) => {
       [email]
     );
 
+    if (rows.length === 0) {
+      return res.status(200).json({message:"No se encontraron tareas", tasks:[]});
+    }
+
     res.status(200).json(rows);
 
   } catch (erro) {
-    console.error("Erro:", erro.message);
     res.status(500).json({ error: "Error interno" });
+  }
+});
+
+// 
+
+// ------- -> DELETE <- -------
+
+router.delete("/todos/:id", authAPI, async (req, res) => {
+  const {email} = req.data;
+  const {id} = req.params;
+
+  try {
+    const [result] = await pool.query(
+      `DELETE t FROM tareas t
+       INNER JOIN users u ON t.user_id = u.id
+       WHERE t.id = ? AND u.email = ?`,
+      [id, email]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({error:"Tarea no encontrada"});
+    }
+
+    res.status(200).json({message:"tarea borrada"});
+
+  } catch (erro) {
+    res.status(500).json({error:"Error interno del servidor"});
   }
 });
 
