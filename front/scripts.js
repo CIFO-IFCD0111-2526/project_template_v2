5c2cb620e6560ac4cef3ce9711712afef041c266
Capitankanalla <nanoxtechnologies@gmail.com>
2026-04-07 18:33:35 +0200

    feat #93. Redirect automático post signup

--- front/scripts.js
+++ front/scripts.js
@@ -165,6 +165,10 @@ forms[1].addEventListener("submit", async (e) => {
     } else {
       signUpRES.textContent = resJSON.message;
       signUpRES.className = "msg_ok";
+      setTimeout(() => {
+        window.location.href = "/signin";          // Redirige a SignIn después de un breve mensaje de éxito. 
+      }, 3000);
+                  
     }
   } catch (error) {
     signUpRES.textContent = "Error de conexión con el servidor.";
